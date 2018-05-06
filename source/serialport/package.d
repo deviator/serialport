0a0db5d96012e5d1160f2d1304b0b2c9c2b6e609
deviator <code.viator@gmail.com>
2018-05-06 22:48:12 +0300

    try fix osx travis build 10

--- source/serialport/package.d
+++ source/serialport/package.d
@@ -196,7 +196,7 @@ void threadTest(SPT)(string[2] ports)
     scope (exit) com.close();
 
     version (linux) enum NN = 0;
-    version (OSX) enum NN = BUFFER_SIZE >> 2;
+    version (OSX) enum NN = BUFFER_SIZE >> 1;
     version (Windows) enum NN = 0;
 
     enum origList = [
